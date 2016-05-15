640c9a4edfadb8b72066bdf7b36afeed034a71e4
Francisco Javier Ugalde <fugalde@rootstack.com>
2016-05-15 18:51:39 -0500

    Some code progress for the api rest example.

---
+++ src/Rootstack/TaskBundle/Form/Type/TaskType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fjugalde
+ * Date: 5/15/16
+ * Time: 4:48 PM
+ */
+
+namespace Rootstack\TaskBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+        $resolver->setDefaults(
+            [
+                'csrf_protection' => false,
+                'data_class' => 'Rootstack\TaskBundle\Entity\Task',
+            ]
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return "add_new_task";
+    }
+
+
+}
