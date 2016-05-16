20d626937819f4f3b88f5b1fc99c7619a8f24ab0
Francisco Javier Ugalde <fugalde@rootstack.com>
2016-05-16 10:41:47 -0500

    PUT Method now is working. Some fixes in file standards.

--- src/Rootstack/TaskBundle/Controller/TaskController.php
+++ src/Rootstack/TaskBundle/Controller/TaskController.php
@@ -13,16 +13,12 @@ use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\Delete;
 use FOS\RestBundle\Controller\Annotations\Put;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\View;
-use Rootstack\TaskBundle\Entity\Task;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Rootstack\TaskBundle\Form\Type\TaskType;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\Util\Codes;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Rootstack\TaskBundle\Entity\Task;
+use Rootstack\TaskBundle\Form\Type\TaskType;
 
 class TaskController extends FOSRestController
 {
@@ -94,8 +90,8 @@ class TaskController extends FOSRestController
      */
     public function putTaskAction(Request $request, Task $task)
     {
-
         $form = $this->createForm(new TaskType(), $task);
+        $form->submit($request);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
@@ -104,7 +100,6 @@ class TaskController extends FOSRestController
             $em->persist($task);
             $em->flush();
 
-//            return $this->view(null, Codes::HTTP_NO_CONTENT);
             return array("task" => $task);
         }
 
@@ -130,48 +125,5 @@ class TaskController extends FOSRestController
 
         return array("status" => "Deleted");
     }
-
-//
-//    /**
-//     * @param Task $task
-//     * @return array
-//     *
-//     * @View()
-//     * * @Post("/task")
-//     */
-//    public function postTaskAction(Request $request)
-//    {
-//        //TODO: there's a simpler method using FOSRestBundle body converter
-//        // that's the reason why we need to be able to create
-//        // an task without body or title, to use it as
-//        // a placeholder for the form
-//        $task = new Task();
-//        // createForm is provided by the parent class
-//        $form = $this->createForm(
-//            new TaskType(),
-//            $task
-//        );
-//        // this method is the one that will use the value in the POST
-//        // to update $task
-//        $form->handleRequest($request);
-//
-//        // we use it like that instead of the standard $form->isValid()
-//        // because the json generated
-//        // is much readable than the one by serializing $form->getErrors()
-//        $errors = $this->get('validator')->validate($task);
-//
-//        if (count($errors) > 0) {
-//            return new View(
-//                array($errors),
-//                Response::HTTP_UNPROCESSABLE_ENTITY
-//            );
-//        }
-//        $manager = $this->getDoctrine()->getManager();
-//        $manager->persist($task);
-//        $manager->flush();
-//        // created => 201
-//        return new View(array($task), Response::HTTP_CREATED);
-//    }
-
 
 }
